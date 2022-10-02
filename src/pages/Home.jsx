d547862b3e59621297b07fc37e6c83a43df6b4e5
Ahmustufa <ahmedmustafa1234@gmail.com>
2022-10-02 22:02:41 +0500

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
+import styled from "styled-components";
 import Loader from "../components/Loader";
 import Navigation from "../components/Navigation";
 const Home = () => {
@@ -8,7 +9,16 @@ const Home = () => {
       <Navigation>
         <Grid container>
           <Grid items xs={12}>
-            Home
+            <Grid container>
+              <BackgroundGrid items xs={12}>
+                <HeaderText variant="h2" style={{ fontWeight: "bold" }}>
+                  CYBER TECH
+                </HeaderText>
+                <Typography variant="h2" style={{ fontWeight: "" }}>
+                  Where Inovation Is Conceived
+                </Typography>
+              </BackgroundGrid>
+            </Grid>
           </Grid>
         </Grid>
       </Navigation>
@@ -17,3 +27,12 @@ const Home = () => {
 };
 
 export default Home;
+
+const BackgroundGrid = styled(Grid)`
+  // background-image: url("https://syspree.com/wp-content/uploads/2020/07/Web-Design-company-in-Mumbai-_-The-Difference-Between-Web-Development-and-Web-Designing-_-SySpree-823x411.jpg");
+  // height: 200px;
+`;
+
+const HeaderText = styled(Typography)`
+  color: #1f6103;
+`;
