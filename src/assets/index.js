b0748e832f7e092a7e8c71b36104778330cb2ae5
Ahmustufa <ahmedmustafa1234@gmail.com>
2022-10-03 23:15:09 +0500

    images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,62 @@
-export const chooseus = [
-    {
-        image: "/"
-    }
-]
+
+export const choose = [
+  {
+    title: "",
+    image: (
+      <img src={require("./choose_1.png")} style={{ objectFit: "cover" }} />
+    ),
+  },
+  {
+    title: "",
+    image: (
+      <img src={require("./choose_2.png")} style={{ objectFit: "cover" }} />
+    ),
+  },
+  {
+    title: "",
+    image: (
+      <img src={require("./choose_3.png")} style={{ objectFit: "cover" }} />
+    ),
+  },
+  {
+    title: "",
+    image: (
+      <img src={require("./choose_4.png")} style={{ objectFit: "cover" }} />
+    ),
+  },
+  {
+    title: "",
+    image: (
+      <img src={require("./choose_5.png")} style={{ objectFit: "cover" }} />
+    ),
+  },
+  {
+    title: "SEO Optimization",
+    image: (
+      <img
+        src={require("./choose_6.png")}
+        style={{ objectFit: "cover", height: "300px" }}
+      />
+    ),
+  },
+];
+// export const choose = [
+//   {
+//     image: "./choose_1.png",
+//   },
+//   {
+//     image: "./choose_2.png",
+//   },
+//   {
+//     image: "./choose_3.png" ,
+//   },
+//   {
+//     image: "./choose_4.png" ,
+//   },
+//   {
+//     image: "./choose_5.png" ,
+//   },
+//   {
+//     image: "./choose_6.png" ,
+//   },
+// ];
--- src/components/WhyChooseUs.js
+++ src/components/WhyChooseUs.js
@@ -1,25 +1,48 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
-const Array = [
-  {
-    title: "Online Store",
-    image: "",
-  },
-];
+import { Button, Card } from "react-bootstrap";
+import styled from "styled-components";
+import { choose } from "../assets/index";
+
 const WhyChooseUs = () => {
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-    >
-      {Array.map((item, index) => (
-        <Grid item xs={2} sm={4} md={4} key={index}>
-          <img src={item.image} />
-        </Grid>
-      ))}
-    </Grid>
+    <Wrapper>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {choose.map((item, index) => (
+          <CardGrid
+            item
+            xs={2}
+            sm={4}
+            md={4}
+            key={index}
+            style={{ flexDirection: "column" }}
+          >
+            <div>{item.image}</div>
+            <Typography variant="h4">{item.title}</Typography>
+          </CardGrid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 };
 
 export default WhyChooseUs;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eeeeee;
+  height: 1000px;
+  width: auto;
+`;
+
+const CardGrid = styled(Grid)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
